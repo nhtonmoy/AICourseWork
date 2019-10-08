422e5231b59a03ca5bcf00dbdf47c7631475e0de
nhtonmoy <38049606+nhtonmoy@users.noreply.github.com>
2019-10-08 22:52:39 +0600

    Update uninformedCostSearch1.cpp

--- uninformedCostSearch1.cpp
+++ uninformedCostSearch1.cpp
@@ -17,27 +17,27 @@ public:
 
 void  ucs(node root,int n)
 {
-    priority_queue<node>pq;
-    pq.push(root);
-    cost[root.x]=root.cost;
-    node temp,t;
+    priority_queue<node>pq; //priority queue of node type is created
+    pq.push(root); //queue te push korlam root object er attributes
+    cost[root.x]=root.cost; //cost array er root.x no element e root.cost insert korlam
+    node temp,t; //temp and t name er duita object create korlam
     int x,y;
 
-    while(!pq.empty())
+    while(!pq.empty()) //queue empty na howa porjonto cholte thakbe loop ta
     {
-        temp=pq.top();
-        pq.pop();
-        x=temp.x;
-        for(int y=1; y<=n; y++)
+        temp=pq.top(); //temp object e queue er top element ta insert hobe (first object er attributes)
+        pq.pop(); //top element pop hoye gelo
+        x=temp.x; //temp object er x attribute ta ke x variable e assign korlam
+        for(int y=1; y<=n; y++) //1st node theke nth node porjonto loop cholbe (main function e je node and edges disilam)
         {
-            if(G[x][y])
+            if(G[x][y]) //x ar y node er path ase ki na
             {
-                if(cost[y]>cost[x]+G[x][y])
+                if(cost[y]>cost[x]+G[x][y]) //y node er total cost ki x node er total cost and x and y er path cost er jogfol er cheye boro ki na
                 {
-                    cost[y]=cost[x]+G[x][y];
-                    t.x=y;
-                    t.cost=cost[y];
-                    pq.push(t);
+                    cost[y]=cost[x]+G[x][y]; //y node er new total cost assign korlam
+                    t.x=y; //t object er x er value assign korlam, new node to be checked
+                    t.cost=cost[y]; //t object er cost e new total cost assign korlam
+                    pq.push(t); //queue te t object ta push kore dilam, t is the new top element
                 }
             }
         }
@@ -83,7 +83,7 @@ int main()
 1 4 3
 2 3 1
 3 5 6
-1 7 8
+3 7 8
 7 5 4
 5 2 4
 4 5 2
